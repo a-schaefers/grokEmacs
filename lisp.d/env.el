4814762f7d089ce2ea1128e74712c2fa9fc141f3
Adam Schaefers <schaefers@riseup.net>
2025-08-15 12:48:08 -0600

    add way to modify PATH

--- lisp.d/env.el
+++ lisp.d/env.el
@@ -7,3 +7,35 @@
 (setenv "EDITOR" "emacsclient")
 (setenv "VISUAL" (getenv "EDITOR"))
 (setenv "PAGER" "cat")
+
+;; 'PATH' modifications
+
+(setq grokemacs-path-insert '("~/bin"
+                            "~/.local/bin"))
+
+(setq grokemacs-path-append '(""))
+
+;; SET matching exec-path and 'PATH' values with inserts/appends
+
+(dolist (item grokemacs-path-insert)
+  (add-to-list 'exec-path item))
+
+(dolist (item grokemacs-path-append)
+  (add-to-list 'exec-path item t))
+
+(setenv "PATH" (string-trim-right (string-join exec-path ":") ":$"));; 'PATH' modifications
+
+(setq grokemacs-path-insert '("~/bin"
+                            "~/.local/bin"))
+
+(setq grokemacs-path-append '(""))
+
+;; SET matching exec-path and 'PATH' values with inserts/appends
+
+(dolist (item grokemacs-path-insert)
+  (add-to-list 'exec-path item))
+
+(dolist (item grokemacs-path-append)
+  (add-to-list 'exec-path item t))
+
+(setenv "PATH" (string-trim-right (string-join exec-path ":") ":$"))
