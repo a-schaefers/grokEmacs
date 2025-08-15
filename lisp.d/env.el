9af5ba1cf11039e1e108ffe00cb20a4df75f081a
Adam Schaefers <schaefers@riseup.net>
2025-08-15 12:49:37 -0600

    remove dupe

--- lisp.d/env.el
+++ lisp.d/env.el
@@ -24,18 +24,3 @@
   (add-to-list 'exec-path item t))
 
 (setenv "PATH" (string-trim-right (string-join exec-path ":") ":$"));; 'PATH' modifications
-
-(setq grokemacs-path-insert '("~/bin"
-                            "~/.local/bin"))
-
-(setq grokemacs-path-append '(""))
-
-;; SET matching exec-path and 'PATH' values with inserts/appends
-
-(dolist (item grokemacs-path-insert)
-  (add-to-list 'exec-path item))
-
-(dolist (item grokemacs-path-append)
-  (add-to-list 'exec-path item t))
-
-(setenv "PATH" (string-trim-right (string-join exec-path ":") ":$"))
