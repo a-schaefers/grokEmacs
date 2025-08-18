57d92277cfb157a8c95172197d23cbc54e6b87a8
Adam Schaefers <schaefers@riseup.net>
2025-08-18 16:40:05 -0600

    proverbial pedantic belt and suspenders

--- early-init.el
+++ early-init.el
@@ -1,3 +1,12 @@
-;;; -*- lexical-binding: t; no-byte-compile: t; -*-
+;;; init.el --- Initialization file for Emacs -*- lexical-binding: t; no-byte-compile: t; -*-
+
+;;; Commentary:
+;; Prevent package.el from interfering with Elpaca by disabling it at startup.
+;; I've observed in some cases it's not enough to do this in regular init.el,
+;; so here we apply it as early as possible for most users.
+
+;;; Code:
 
 (setq package-enable-at-startup nil)
+
+;;; early-init.el ends here
