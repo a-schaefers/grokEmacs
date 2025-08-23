d747e11cb6dd3d01a501b345288e70bdb4d8d655
Adam Schaefers <schaefers@riseup.net>
2025-08-22 23:12:11 -0600

    Remove commented code block

--- grok-core/grok-treesit.el
+++ grok-core/grok-treesit.el
@@ -2,15 +2,6 @@
 
 ;; TODO: https://github.com/mickeynp/combobulate check this out
 
-;; (use-package treesit-auto
-;;   :ensure t
-;;   :demand t
-;;   :custom
-;;   (treesit-auto-install 'prompt)
-;;   :config
-;;   (treesit-auto-add-to-auto-mode-alist 'all)
-;;   (global-treesit-auto-mode))
-
 (use-package treesit-auto
   :ensure t
   :hook (elpaca-after-init . global-treesit-auto-mode)
