cc9d0309fea2152a5f6021923f2e2b010f901723
Adam Schaefers <schaefers@riseup.net>
2025-08-24 21:19:40 -0600

    ADd projectile-command-map

--- grok-core/grok-projectile.el
+++ grok-core/grok-projectile.el
@@ -7,6 +7,7 @@
   :init
   (setq projectile-project-search-path `((,grok-projects . 1)))
   :hook (elpaca-after-init . projectile-mode)
+  :bind (("C-c p" . projectile-command-map)) ; already on SPC and M-m but people I think expect this to be there
   :config
   (projectile-discover-projects-in-search-path)
   (projectile-save-known-projects))
